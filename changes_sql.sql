df4c8adf3e17f50a8e5c72873e27ad62bc1cd950
Avani Dave <avani.dave@innvonix.com>
2021-04-14 23:24:08 +0530

    migrations and added column

--- changes_sql.sql
+++ changes_sql.sql
@@ -2,3 +2,6 @@
 ALTER TABLE `users` ADD `birth_date` DATE NULL DEFAULT NULL AFTER `gender`;
 ALTER TABLE `chat_groups` CHANGE `group_name` `group_name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
 ALTER TABLE `chat_groups` CHANGE `group_image` `group_image` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+
+/* 14-04-2021 */
+ALTER TABLE `users` CHANGE `is_hi` `is_hi` TINYINT(20) NULL DEFAULT '0' COMMENT ' 0 = no , 1 = yes';

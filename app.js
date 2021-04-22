1345581c5866c4a57ae90ea37689354627719a4f
Avani Dave <avani.dave@innvonix.com>
2021-04-22 22:18:28 +0530

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const http = require('http');
 
 // create new express app and save it as "app"
 const app = express();
-app.use(cors());
+app.use(cors({}));
 // app.use(express.json());
 
 const httpServer = http.createServer(app);
